3587e413490bd3f5b4cd4b8146b2722e1c90d4f6
uknow <yunhoknight@naver.com>
2023-09-23 21:22:00 +0900

    Initial commit

---
+++ src/main/java/com/fastcampus/projectadmin/FastcampusProjectAdminApplication.java
@@ -0,0 +1,13 @@
+package com.fastcampus.projectadmin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FastcampusProjectAdminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FastcampusProjectAdminApplication.class, args);
+    }
+
+}
